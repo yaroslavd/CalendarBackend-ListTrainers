bd129a6d3f8c12f7147184e2de6ea4f5adefc51d
Yaroslav Dvinov <yd43@cornell.edu>
2015-08-31 21:20:34 -0700

    Changing FreeSlotsRequest

--- src/personal/dvinov/calendar/service/api/freeslots/FreeSlotsRequest.java
+++ src/personal/dvinov/calendar/service/api/freeslots/FreeSlotsRequest.java
@@ -1,51 +1,69 @@
 package personal.dvinov.calendar.service.api.freeslots;
 
+import java.time.Instant;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class FreeSlotsRequest {
-    private Day inclusiveStartDay;
-    private Day inclusiveEndDay;
+    private Instant queryIntervalStart;
+    private Instant queryIntervalEnd;
+    private String location;
     private String trainerId;
     
     public FreeSlotsRequest() {
         
     }
     
-    public FreeSlotsRequest(final Day inclusiveStartDay, final Day inclusiveEndDay, final String trainerId) {
-        this.inclusiveStartDay = inclusiveStartDay;
-        this.inclusiveEndDay = inclusiveEndDay;
+    public FreeSlotsRequest(
+            final Instant queryIntervalStart,
+            final Instant queryIntervalEnd,
+            final String location,
+            final String trainerId) {
+        
+        this.queryIntervalStart = queryIntervalStart;
+        this.queryIntervalEnd = queryIntervalEnd;
+        this.location = location;
         this.trainerId = trainerId;
     }
 
-    public Day getInclusiveStartDay() {
-        return inclusiveStartDay;
+    public Instant getQueryIntervalStart() {
+        return queryIntervalStart;
     }
 
-    public void setInclusiveStartDay(Day inclusiveStartDay) {
-        this.inclusiveStartDay = inclusiveStartDay;
+    public void setQueryIntervalStart(final String queryIntervalStart) {
+        this.queryIntervalStart = Instant.parse(queryIntervalStart);
     }
 
-    public Day getInclusiveEndDay() {
-        return inclusiveEndDay;
+    public Instant setQueryIntervalEnd() {
+        return queryIntervalEnd;
     }
 
-    public void setInclusiveEndDay(Day inclusiveEndDay) {
-        this.inclusiveEndDay = inclusiveEndDay;
+    public void setQueryIntervalEnd(final String queryIntervalEnd) {
+        this.queryIntervalEnd = Instant.parse(queryIntervalEnd);
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public String getTrainerId() {
         return trainerId;
     }
 
-    public void setTrainerId(String trainerId) {
+    public void setTrainerId(final String trainerId) {
         this.trainerId = trainerId;
     }
     
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .append(inclusiveStartDay)
-                .append(inclusiveEndDay)
+                .append(queryIntervalStart)
+                .append(queryIntervalEnd)
+                .append(location)
                 .append(trainerId)
         .build();
     }
